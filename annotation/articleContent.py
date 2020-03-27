930b43f6d2085b9502663e49fe9d24608180b922
nakulkp <nakulkp@gmail.com>
2020-03-27 16:46:27 +0530

    'debug'

--- annotation/articleContent.py
+++ annotation/articleContent.py
@@ -9,12 +9,12 @@ def articleContent(requestParameters):
 
     flag = int(flag)
 
-    #params = config()
-    #conn = psycopg2.connect(**params)
+    # params = config()
+    # conn = psycopg2.connect(**params)
     conn = psycopg2.connect(host="localhost", database="annotation", user="postgres", password="pass")
     cur = conn.cursor()
     userExists = cur.execute("SELECT exists (SELECT 1 FROM users WHERE user_id = %(user_id)s LIMIT 1);",
-                                {'user_id': user_id})
+                             {'user_id': user_id})
     userExists = cur.fetchone()
     userExists = userExists[0]
     if not userExists:
@@ -31,7 +31,7 @@ def articleContent(requestParameters):
     cur.execute(
         """SELECT owner, release_date, source, url, headline, content, question 
         FROM master_table 
-        WHERE article_id= %(article_id)s AND status=todo;""",
+        WHERE article_id= %(article_id)s AND status="todo";""",
         {"article_id": article_id}
     )
     row = cur.fetchall()
@@ -43,33 +43,41 @@ def articleContent(requestParameters):
     content = row[0][5]
     question = row[0][6]
 
-    cur.execute(
-        """SELECT country_id, commodity_id, category_id, subcategory_id, moving_factor_id, factor_value_id, price_value_id, supply_value_id, demand_value_id, sc_disruption_value_id 
-        FROM mapping_table 
-        WHERE article_id= %(article_id)s;""", {"article_id": article_id}
-    )
-
-    row = cur.fetchall()
-    country_id = row[0][0]
-    commodity_id = row[0][1]
-    category_id = row[0][2]
-    subcategory_id = row[0][3]
-    moving_factor_id = row[0][4]
-    factor_value_id = row[0][5]
-    price_value_id = row[0][6]
-    supply_value_id = row[0][7]
-    demand_value_id = row[0][8]
-    sc_disruption_value_id = row[0][9]
-
-    cur.close()
-    conn.commit()
     returnList = {'owner': owner, 'release_date': release_date, 'source': source, 'url': url, 'headline': headline,
-                    'content': content, 'question': question, 'article_id': article_id, 'country_id': country_id,
+                  'content': content, 'question': question, 'article_id': article_id}
+
+    cur.execute("""SELECT exists (SELECT 1 FROM mapping_table
+            WHERE article_id= %(article_id)s LIMIT 1);""", {'article_id': article_id})
+
+    articleExist = cur.fetchone()
+    articleExist = articleExist[0]
+    if articleExist:
+        cur.execute(
+            """SELECT country_id, commodity_id, category_id, subcategory_id, moving_factor_id, factor_value_id, price_value_id, supply_value_id, demand_value_id, sc_disruption_value_id 
+            FROM mapping_table 
+            WHERE article_id= %(article_id)s;""", {"article_id": article_id}
+        )
+
+        row = cur.fetchall()
+        country_id = row[0][0]
+        commodity_id = row[0][1]
+        category_id = row[0][2]
+        subcategory_id = row[0][3]
+        moving_factor_id = row[0][4]
+        factor_value_id = row[0][5]
+        price_value_id = row[0][6]
+        supply_value_id = row[0][7]
+        demand_value_id = row[0][8]
+        sc_disruption_value_id = row[0][9]
+
+        extraVal = {'country_id': country_id,
                     'commodity_id': commodity_id, 'category_id': category_id, 'subcategory_id': subcategory_id,
                     'moving_factor_id': moving_factor_id, 'factor_value_id': factor_value_id,
                     'price_value_id': price_value_id, 'supply_value_id': supply_value_id,
                     'demand_value_id': demand_value_id, 'sc_disruption_value_id': sc_disruption_value_id}
 
+        returnList.update(extraVal)
+
+    cur.close()
+    conn.commit()
     return returnList
-    if conn is not None:
-        conn.close()
