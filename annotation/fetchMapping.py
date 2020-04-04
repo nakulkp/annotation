35137a8200f66b0919a70381b9f9e2553e7b77e8
vaisakhjnr <vaisakh@inskade.com>
2020-04-04 13:25:48 +0530

    Fixed bug in line 32 fetchmapping.py

--- annotation/fetchMapping.py
+++ annotation/fetchMapping.py
@@ -29,18 +29,18 @@ def fetchMapping(requestParameters):
     returnList = []
 
     for row in rows:
-        country_id = row[0][0]
-        commodity_id = row[0][1]
-        category_id = row[0][2]
-        subcategory_id = row[0][3]
-        moving_factor_id = row[0][4]
-        factor_value_id = row[0][5]
-        price_value_id = row[0][6]
-        supply_value_id = row[0][7]
-        demand_value_id = row[0][8]
-        sc_disruption_value_id = row[0][9]
-        mapping_id = row[0][10]
-        user_id = row[0][11]
+        country_id = row[0]
+        commodity_id = row[1]
+        category_id = row[2]
+        subcategory_id = row[3]
+        moving_factor_id = row[4]
+        factor_value_id = row[5]
+        price_value_id = row[6]
+        supply_value_id = row[7]
+        demand_value_id = row[8]
+        sc_disruption_value_id = row[9]
+        mapping_id = row[10]
+        user_id = row[11]
 
         loopVal = {'mapping_id': mapping_id, 'user_id': user_id, 'country_id': country_id,
                    'commodity_id': commodity_id, 'category_id': category_id, 'subcategory_id': subcategory_id,
