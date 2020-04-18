634822b202b04608b2e794d9b54a7926bdb6e785
Vaisakh J Nair <vaisakhjnr@gmail.com>
2020-04-18 18:57:15 +0530

    Updated review to take factor input and return date in desc

--- annotation/review.py
+++ annotation/review.py
@@ -12,14 +12,16 @@ def review(requestParameters):
     cur = conn.cursor()
 
     page = requestParameters['page']
-    offset = (page-1)*10
-    limit = offset + 10
+    factor = requestParameters['factor']
+
+    offset = (page-1)*factor
+    limit = offset + factor
 
     cur.execute("""SELECT COUNT(article_id) FROM master_table;""")
     dataCount = cur.fetchall()
     dataCount = dataCount[0]
-    pageCount = dataCount[0]//10
-    if (dataCount[0] % 10) != 0 and dataCount[0] > 10:
+    pageCount = dataCount[0]//factor
+    if (dataCount[0] % factor) != 0 and dataCount[0] > factor:
         pageCount = pageCount + 1        
 
     cur.execute("SELECT privilege FROM users WHERE user_id = %(user_id)s; ",
@@ -28,14 +30,14 @@ def review(requestParameters):
     privilege = privilege[0]
 
     if privilege == '1':
-        cur.execute("""SELECT article_id, headline, status, question, url
-            FROM master_table LIMIT %(limit)s OFFSET %(offset)s;""", {"limit": limit, "offset": offset})
+        cur.execute("""SELECT article_id, headline, status, question, url, created_date
+            FROM master_table LIMIT %(limit)s OFFSET %(offset)s ORDER BY created_date DESC;""", {"limit": limit, "offset": offset})
         reviewValues = cur.fetchall()
 
     else:
-        cur.execute("""SELECT article_id, headline, status, question, url
+        cur.execute("""SELECT article_id, headline, status, question, url, created_date
                     FROM master_table
-                    WHERE user_id=%(user_id)s LIMIT %(limit)s OFFSET %(offset)s;""", {'user_id': user_id, "limit": limit, "offset": offset})
+                    WHERE user_id=%(user_id)s LIMIT %(limit)s OFFSET %(offset)s ORDER BY created_date DESC;""", {'user_id': user_id, "limit": limit, "offset": offset})
         reviewValues = cur.fetchall()
 
     cur.close()
