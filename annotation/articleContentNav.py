3d1f35cf1db17f390bad4b46d668058fb139cce1
Vaisakh J Nair <vaisakhjnr@gmail.com>
2020-04-19 13:05:07 +0530

    Fixed articleContentNav

--- annotation/articleContentNav.py
+++ annotation/articleContentNav.py
@@ -14,16 +14,19 @@ def articleContentNav(requestParameters):
     cur = conn.cursor()
 
     if direction == 0:
-        cur.execute("""SELECT COUNT(article_id)
-            FROM master_table
-            WHERE article_id <= %(article_id)s;""", {"article_id": article_id})
-        todoCount = cur.fetchone()
-        todoCount = todoCount[0]
+        if article_id != 1:
+            article_id -= article_id
+            cur.execute("""SELECT COUNT(article_id) FROM master_table WHERE article_id = %(article_id)s;""", {"article_id": article_id})
+            todoCount = cur.fetchone()
+        else:
+            article_id -= article_id
+            cur.execute("""SELECT COUNT(article_id) FROM master_table WHERE article_id <= %(article_id)s;""", {"article_id": article_id})
+            todoCount = cur.fetchone()
+        todoCount = todoCount[0]        
 
     elif direction == 1:
-        cur.execute("""SELECT COUNT(article_id)
-            FROM master_table
-            WHERE article_id >= %(article_id)s;""", {"article_id": article_id})
+        article_id += article_id
+        cur.execute("""SELECT COUNT(article_id) FROM master_table WHERE article_id >= %(article_id)s;""", {"article_id": article_id})
         todoCount = cur.fetchone()
         todoCount = todoCount[0]
 
