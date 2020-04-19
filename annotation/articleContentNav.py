da1340405de976b8808ae84aa0dcbb08fc46007d
Vaisakh J Nair <vaisakhjnr@gmail.com>
2020-04-19 11:44:24 +0530

    Ficed articleContentNav

--- annotation/articleContentNav.py
+++ annotation/articleContentNav.py
@@ -13,11 +13,30 @@ def articleContentNav(requestParameters):
     conn = psycopg2.connect(host="localhost", database="annotation", user="postgres", password="pass")
     cur = conn.cursor()
 
-    cur.execute("""SELECT COUNT(article_id)
-        FROM master_table
-        WHERE article_id = %(article_id)s;""", {"article_id": article_id})
-    todoCount = cur.fetchone()
-    todoCount = todoCount[0]
+    todoCount = 0
+
+    if direction == 0:
+        if article_id != 1:
+            article_id -= article_id
+            cur.execute("""SELECT COUNT(article_id)
+                FROM master_table
+                WHERE article_id <= %(article_id)s;""", {"article_id": article_id})
+            todoCount = cur.fetchone()
+        else:
+            article_id += article_id
+            cur.execute("""SELECT COUNT(article_id)
+                FROM master_table
+                WHERE article_id = %(article_id)s;""", {"article_id": article_id})
+            todoCount = cur.fetchone()
+        todoCount = todoCount[0]        
+
+    elif direction == 1:
+        article_id += article_id
+        cur.execute("""SELECT COUNT(article_id)
+            FROM master_table
+            WHERE article_id >= %(article_id)s;""", {"article_id": article_id})
+        todoCount = cur.fetchone()
+        todoCount = todoCount[0]
 
     if todoCount == 0:
         return {"message": "empty"}
